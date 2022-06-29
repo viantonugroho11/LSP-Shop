e23cbb56711e58a14b33e45bf04a6213a92e3d6d
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-06-29 08:59:28 +0700

    add resource

--- app/Http/Controllers/Backend/Auth/AuthController.php
+++ app/Http/Controllers/Backend/Auth/AuthController.php
@@ -4,8 +4,51 @@ namespace App\Http\Controllers\Backend\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class AuthController extends Controller
 {
-    //
+    // use AuthenticatesUsers;
+
+    protected $maxAttempts = 3;
+    protected $decayMinutes = 2;
+
+    public function __construct()
+    {
+        $this->middleware('guest:admins')->except('postLogout');
+    }
+
+    public function getLogin()
+    {
+        return view('auth.admin.login');
+    }
+
+    public function postLogin(Request $request)
+    {
+        $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required|min:5'
+        ]);
+
+        if (auth()->guard('admins')->attempt($request->only('email', 'password'))) {
+            $request->session()->regenerate();
+            $this->clearLoginAttempts($request);
+            return redirect()->intended();
+        } else {
+            $this->incrementLoginAttempts($request);
+
+            return redirect()
+                ->back()
+                ->withInput()
+                ->withErrors(["Incorrect user login details!"]);
+        }
+    }
+
+    public function postLogout()
+    {
+        auth()->guard('admins')->logout();
+        session()->flush();
+
+        return redirect()->route('admin.login');
+    }
 }
