d16120fc084aac8f56beca3ff936fb2b048e25c0
Muhammad Novianto Nugroho <viantonugroho@student.gunadarma.ac.id>
2022-07-01 18:30:03 +0700

    add fe

--- app/Http/Controllers/Frontend/Home/HomeController.php
+++ app/Http/Controllers/Frontend/Home/HomeController.php
@@ -3,9 +3,17 @@
 namespace App\Http\Controllers\Frontend\Home;
 
 use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    public function index()
+    {
+        $categorylist = Category::all();
+        $productlatest = Product::latest()->limit(5);
+        
+        return view('frontend.home.index');
+    }
 }
